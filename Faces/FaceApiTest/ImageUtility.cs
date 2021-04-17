a4e537c65c1cfd897ce41c88dc9d8d2f2621bc64
Lucas R Sena <msbowelite@gmail.com>
2021-04-17 20:34:01 -0300

    Initialize poc

---
+++ Faces/FaceApiTest/ImageUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceApiTest
+{
+    public class ImageUtility
+    {
+        public byte[] ConvertToBytes(string imagePath)
+        {
+            MemoryStream memoryStream = new ();
+            using FileStream fileStream = new(imagePath, FileMode.Open);
+
+            fileStream.CopyTo(memoryStream);
+
+            var bytes = memoryStream.ToArray();
+            return bytes;
+        }
+
+        public void FromBytesToImage(byte[] imageBytes, string fileName)
+        {
+            using MemoryStream memoryStram = new(imageBytes);
+
+            Image img = Image.FromStream(memoryStram);
+            img.Save(fileName + ".jpg", ImageFormat.Jpeg);
+        }
+    }
+}
---
+++ Faces/FaceApiTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FaceApiTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var imagePath = @"photo.jpg";
+            var urlAddress = "http://localhost:6000/api/faces";
+
+            ImageUtility imageUtility = new();
+            var bytes = imageUtility.ConvertToBytes(imagePath);
+            List<byte[]> faceList = null;
+
+            var byteContent = new ByteArrayContent(bytes);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            using HttpClient httpClient = new();
+            using var response = await httpClient.PostAsync(urlAddress, byteContent);
+
+            var apiResponse = await response.Content.ReadAsStringAsync();
+            faceList = JsonSerializer.Deserialize<List<byte[]>>(apiResponse);
+
+            if(faceList is not null)
+            {
+                for(int i=0; i<faceList.Count; i++)
+                {
+                    imageUtility.FromBytesToImage(faceList[i], "face" + i);
+                }
+            }
+        }
+    }
+}
